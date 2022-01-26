ffef819cb1c9d6419f9523856e4ecfe6601cebb0
LeonardoLuize <leonardo.luize2@gmail.com>
2022-01-25 21:11:08 -0300

    Paginação com miragejs e substituição do fetch pelo axios

--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -5,29 +5,11 @@ import { RiAddLine, RiPencilLine } from "react-icons/ri";
 import Header from "../../components/Header";
 import Pagination from "../../components/Pagination";
 import SideBar from "../../components/SideBar";
-import { useQuery } from 'react-query';
+import { useUsers } from "../../services/hooks/useUsers";
 
 export default function UserList() {
 
-    const { data, isLoading, error } = useQuery('users', async () => {
-        const res = await fetch('http://localhost:3000/api/users');
-        const data = await res.json();
-
-        const users = data.users.map(user => {
-            return {
-                id: user.id,
-                name: user.name,
-                email: user.email,
-                createdAt: new Date(user.createdAt).toLocaleDateString('pt-BR', {
-                    day: '2-digit',
-                    month: 'long',
-                    year: 'numeric',
-                })
-            }
-        });
-
-        return users;
-    });
+    const { data, isLoading, isFetching, error } = useUsers();
 
     const isWideVersion = useBreakpointValue({
         base: false,
@@ -48,7 +30,9 @@ export default function UserList() {
 
                 <Box flex="1" borderRadius={8} bg="gray.800" p="8">
                     <Flex mb="8" justify="space-between" align="center">
-                        <Heading size="lg" fontWeigth="normal">Listagem de Usuários</Heading>
+                        <Heading size="lg" fontWeigth="normal">Listagem de Usuários
+                            {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
+                        </Heading>
 
                         <Link href="/users/create" passHref>
                             <Button as="a" size="sm" fontSize="sm" colorScheme="pink" leftIcon={<Icon fontSize="20" as={RiAddLine} />}>
